174fe1e60c3ca40f825f234f4576bdca600eece2
Jonnathan Gonzalez <137486132+josejonnathan@users.noreply.github.com>
2024-09-13 19:58:26 +0200

    Update settings.py

--- jg2portfolio/settings.py
+++ jg2portfolio/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'jg2portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'procurvit$default',
+        'USER': 'procurvit',
+        'PASSWORD':config('DB_PASSWORD'),
+        'HOST': 'procurvit.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
